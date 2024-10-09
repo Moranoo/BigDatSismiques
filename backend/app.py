5b05da10b7a415313d23924418caa5f69b55591d
Moranoo <92519044+Moranoo@users.noreply.github.com>
2024-10-09 09:52:59 +0200

    Projet Init

---
+++ backend/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+from kafka import KafkaProducer
+from marshmallow import Schema, fields, ValidationError
+import json
+
+app = Flask(__name__)
+
+producer = KafkaProducer(
+    bootstrap_servers='kafka:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+class SismiqueSchema(Schema):
+#  mettre les colonnes du schéma sismique ici
+
+
+sismique_schema = SismiqueSchema()
+
+@app.route('/sismique', methods=['POST'])
+def add_user():
+    json_data = request.get_json()
+    if not json_data:
+        return jsonify({"message": "No input data provided"}), 400
+
+    try:
+        data = sismique_schema.load(json_data)
+    except ValidationError as err:
+        return jsonify(err.messages), 422
+
+    producer.send('topic1', data)
+    producer.flush()
+
+    return jsonify({"message": "Sismique data received and sent to Kafka"}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5550)
